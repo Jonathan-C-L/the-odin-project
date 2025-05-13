0b9784cef91cf275e2f10b689b83089a5b75e4ab
Jonathan <jle16@nait.ca>
2025-05-13 14:07:44 -0600

    general html and images completed - currently working on styling

---
+++ rock-paper-scissors/js.js
@@ -0,0 +1,21 @@
+console.log("Hello world!");
+preloadImages = [];
+/****************psuedo-class***************/
+function Choice(RPS, alt){
+    this.choice = RPS;
+    this.icon.src = `${RPS}.png`;
+    this.icon.alt = alt
+}
+
+/******************functions***************/
+// preloading images
+function imgInit(){
+
+}
+function getComputerChoice(){
+
+}
+
+function getUserChoice(){
+
+}
