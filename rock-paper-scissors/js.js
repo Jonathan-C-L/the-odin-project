a7891bf2c398b4c57e15a92761a6ba637cc5c04a
Jonathan <jle16@nait.ca>
2025-05-14 15:23:29 -0600

    computer and human choice functions return values - now  must verify and compare

--- rock-paper-scissors/js.js
+++ rock-paper-scissors/js.js
@@ -1,11 +1,19 @@
+/**************global items*****************/
 console.log("Hello world!");
 preloadImages = [];
+
 /****************event handler**************/
 window.onload = () => { 
+    // variables
+    const choice = document.querySelectorAll("button");
+    const user = document.querySelector(".user-selection");
+
+    
     // preloading images
     imgInit();
 
     // get user choice
+    getUserChoice();
 
 }
 /******************functions***************/
@@ -14,7 +22,7 @@ function imgInit(){
     for(let i  = 0; i < 3; i++){
         // constructing new image object for each image
         let tempImage = new Image();
-        // using switch statements to try it out
+        // using switch statements to try it out - setting src and alt
         switch(i){
             case 0:
                 tempImage.src = `./images/paper.png`
@@ -34,13 +42,44 @@ function imgInit(){
     }
 }
 function getComputerChoice(){
+    // randomize function needs to include 2, so max was +1
+    return Randomize(0, 3);
+}
 
+// randomize helper function to get random numbers between 0-2
+function Randomize(min, max){
+    // generates random number between min(inclusive) and max(exclusive)
+    return Math.floor(Math.random()*(max-min))+min;
 }
 
 function getUserChoice(){
-    document.getElementsByClassName("user-selection").
+    const user = document.querySelector(".user-selection");
+    const choice = document.querySelectorAll("button");
+
+    user.style.display = "flex";
+
+    // choice made
+    for(let i = 0; i < choice.length; i++){
+        choice[i].onclick = () =>{
+            user.style.display = "none";
+            if(i == 0){
+                console.log("user chose rock");
+                return 0;
+            }
+            else if (i == 1){
+                console.log("user chose paper");
+                return 1;
+            }
+            else{
+                console.log("user chose scissors")
+                return 2;
+            }
+        }
+    }
 }
 
 function playRound(){
+    const matchup = document.querySelector(".matchup");
+    matchup.style.display = "flex";
 
 }
