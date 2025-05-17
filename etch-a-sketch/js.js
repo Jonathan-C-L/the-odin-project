e9575c78f36e2f993ba58af4e469a8d34991719e
Jonathan <jle16@nait.ca>
2025-05-17 17:21:43 -0600

    currently working on deleting the previous grid when the reset button is clicked - need to figure out how to remove child elelements

--- etch-a-sketch/js.js
+++ etch-a-sketch/js.js
@@ -1,10 +1,49 @@
+let columns = rows = 16;
 
 // onload
 window.onload = () =>{
-    const main = document.querySelector(".container");
-    const columns = 16, rows = 16;
 
-    // creating the 16x16 grid by creating each column, then populating the rows
+    // creating the 16x16 grid of squares (only on initial load)
+    generateGrid(rows, columns);
+
+    // drawing on the canvas
+    const squares = document.querySelectorAll(".grid");
+    squares.forEach((square)=>{
+        square.addEventListener("mouseover", drawing);
+    });
+
+    // reset button
+    const button = document.querySelector(".reset");
+    button.addEventListener("click", reset);
+};
+
+function reset(){
+    // remove old grid
+    const container = document.querySelectorAll(".container");
+
+    for (col = 0; col < columns; col++){
+        const prevCol = document.createElement("div");
+
+        for(row = 0; row < rows; row++){
+            const prevRow = document.createElement("div");
+
+            prevRow.classList.add("grid", `${col}${row}`);
+            prevCol.appendChild(prevRow);
+        }
+    }
+
+
+    let columns = rows = window.prompt("Specify the size of the new canvas: ", 16);
+
+
+    // regenerate grid with specified size
+    generateGrid(rows, columns);
+    
+}
+
+function generateGrid(rows, columns){
+    const main = document.querySelector(".container");
+
     for (col = 0; col < columns; col++){
         const newCol = document.createElement("div");
 
@@ -16,15 +55,11 @@ window.onload = () =>{
         }
         main.appendChild(newCol);
     }
+}
 
-    // hover event
-    const squares = document.querySelectorAll(".grid");
-    squares.forEach((square)=>{
-        square.addEventListener("mouseover", drawing);
-    });
-};
+function deleteGrid(){
 
-
+}
 
 function drawing(){
     this.classList.add("drawn");
