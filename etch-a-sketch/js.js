523c1600c7e4aea49f14b52ba1ba0fd722b9cb07
Jonathan <jle16@nait.ca>
2025-05-18 16:30:18 -0600

    functionality of etch-a-sketch is complete - optional styling can be done at a later time

--- etch-a-sketch/js.js
+++ etch-a-sketch/js.js
@@ -2,15 +2,10 @@ const defaultSize = 16;
 
 // onload
 window.onload = () =>{
-    const squares = document.querySelectorAll(".grid");
-
     // creating the 16x16 grid of squares (only on initial load)
     generateGrid(defaultSize, defaultSize);
 
-    // drawing on the canvas
-    squares.forEach((square)=>{
-        square.addEventListener("mouseover", drawing);
-    });
+    draw();
 
     // reset button
     const button = document.querySelector(".reset");
@@ -24,13 +19,22 @@ function reset(){
     gridContainer.innerHTML = "";
 
     let newSize = window.prompt("Specify the size of the new canvas: ", 16);
+    console.log(newSize);
     if(newSize > 100){
         window.alert("The size cannot exceed 100!");
-        reset();
     }
-
-    // regenerate grid with specified size
-    generateGrid(newSize, newSize);
+    else if(newSize <= 0){
+        window.alert("The size must be greater than 0!");
+        // if prompt is cancelled - regenerate at the default size
+        generateGrid(defaultSize, defaultSize);
+    }
+    else if(newSize <= 100 && newSize > 0){
+        // regenerate grid with specified size
+        generateGrid(newSize, newSize);
+    }
+    // needs to be re-called to be able to draw again
+    draw();
+ 
 }
 
 function generateGrid(rows, columns){
@@ -54,4 +58,12 @@ function generateGrid(rows, columns){
 function drawing(){
     this.classList.add("drawn");
     console.log("square changed to drawn");
+}
+
+// drawing on the canvas
+function draw(){
+    const squares = document.querySelectorAll(".grid");
+    squares.forEach((square)=>{
+        square.addEventListener("mouseover", drawing)
+    });
 }
