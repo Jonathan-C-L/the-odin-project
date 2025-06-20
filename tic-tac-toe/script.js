53ebefea34cc8be10dd34e551a63e57abbbb5245
Jonathan-C-L <jle16@nait.ca>
2025-06-20 17:00:45 -0600

    added in game tie

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -3,6 +3,7 @@ console.log("Welcome to tic tac toe!");
 
 // factory functions
 function createGameboard(){
+    // 2D array for the gameboard
     let board = [
         ['_', '_', '_'], 
         ['_', '_', '_'], 
@@ -24,14 +25,25 @@ function createGameboard(){
         console.log(board[2][0] + ` | ` + board[2][1] + ` | ` + board[2][2]);
         
         // winner check
-        if(
-            board[0][0] == player.mark && board[1][0] == player.mark && board[2][0] == player.mark ||
+        if( board[0][0] == player.mark && board[1][0] == player.mark && board[2][0] == player.mark ||
             board[0][1] == player.mark && board[1][1] == player.mark && board[2][1] == player.mark ||
             board[0][2] == player.mark && board[1][2] == player.mark && board[2][2] == player.mark ||
             board[0][0] == player.mark && board[1][1] == player.mark && board[2][2] == player.mark ||
             board[2][0] == player.mark && board[1][1] == player.mark && board[0][2] == player.mark
         ){
             console.log(`${player.name} is the winner!`);
+        }
+        // tie check
+        let positionsLeft = 0;
+        for(let i = 0; i < board.length; i++){
+            for(let j = 0; j < board[i].length; j++){
+                if(board[i][j] === '_'){
+                    positionsLeft++;
+                }
+            }
+        }
+        if(positionsLeft <= 0){
+            console.log(`No more moves, tie game!`);
         }
     };  
 
