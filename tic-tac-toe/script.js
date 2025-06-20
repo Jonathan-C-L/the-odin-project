6ec71dbf605c1c31f48c73830b8d8dc30be70748
Jonathan-C-L <jle16@nait.ca>
2025-06-20 14:55:45 -0600

    game logic complete - every win condition has been verified; out of bounds choices verified

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,17 +1,41 @@
 console.log("Welcome to tic tac toe!");
 
 
-
-
-
-
 // factory functions
 function createGameboard(){
-    let board = [,];
+    let board = [
+        ['_', '_', '_'], 
+        ['_', '_', '_'], 
+        ['_', '_', '_']
+    ];
 
-    const pickPosition = (x, y, mark) => {
-        (board[x, y] === undefined)?board[x, y] = mark:console.log("Position is already taken!");
+    const pickPosition = (x, y, player) => {
+        // validate that position chosen is valid - positions < 0 automatically throw error
+        if(x < 3 && y < 3){
+            (board[x][y] === '_')?board[x][y] = player.mark:console.log("Position is already taken!");
+        }
+        else{
+            console.log("Position must be between 0 and 2!");
+        }
+
+        // display board
+        console.log(board[0][0] + ` | ` + board[0][1] + ` | ` + board[0][2]);
+        console.log(board[1][0] + ` | ` + board[1][1] + ` | ` + board[1][2]);
+        console.log(board[2][0] + ` | ` + board[2][1] + ` | ` + board[2][2]);
+        
+        // winner check
+        if(
+            board[0][0] == player.mark && board[1][0] == player.mark && board[2][0] == player.mark ||
+            board[0][1] == player.mark && board[1][1] == player.mark && board[2][1] == player.mark ||
+            board[0][2] == player.mark && board[1][2] == player.mark && board[2][2] == player.mark ||
+            board[0][0] == player.mark && board[1][1] == player.mark && board[2][2] == player.mark ||
+            board[2][0] == player.mark && board[1][1] == player.mark && board[0][2] == player.mark
+        ){
+            console.log(`${player.name} is the winner!`);
+        }
     };  
+
+    return {board, pickPosition};
 }
 
 function createPlayer(name, mark){
