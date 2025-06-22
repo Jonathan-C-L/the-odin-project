14b453e910e390ba4f6243a827651d5c8084c09a
Jonathan-C-L <jle16@nait.ca>
2025-06-22 14:35:16 -0600

    messages pop up and disappear at the correct times now, now working on clearing the board when the reset button is clicked

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -4,27 +4,16 @@ const player2 = createPlayer('', 'o');
 
 window.addEventListener("load", () =>{
     const button = document.querySelector(".submit");
-    const playerInput = document.querySelector(".player-input");
     const player1Input = document.querySelector("#player1-input");
-    const player2Input = document.querySelector("#player2-input");
-    const playerDisplay = document.querySelector(".player-display");
-    const player1Display = document.querySelector(".player1");
-    const player2Display = document.querySelector(".player2");
+    const player2Input = document.querySelector("#player2-input");    
     const board = document.querySelectorAll(".board");
+    const reset = document.querySelector(".reset");
 
     console.log("Welcome to tic tac toe!");
 
     button.addEventListener("click", function(){        
         if(player1Input.value != "" && player2Input.value != ""){
-            // adding user input into UI
-            player1.name = player1Input.value;
-            player2.name = player2Input.value;
-            player1Display.textContent = "Player 1: " + player1.name;
-            player2Display.textContent = "Player 2: " + player2.name;
-            // hiding input elements
-            playerInput.classList.toggle("hidden");
-            button.classList.toggle("hidden");
-            playerDisplay.classList.toggle("hidden");
+            DisplayPlayers();
             gameboard.playGame();
         }
         else{
@@ -38,20 +27,28 @@ window.addEventListener("load", () =>{
             if(gameboard.checkGame()){
                 if(gameboard.getTurn()%2 == 0){
                     gameboard.pickPosition(i, player1);
+                    DisplayBoard();
                     CheckBoard(player1);
                 }
                 else{
                     gameboard.pickPosition(i, player2);
+                    DisplayBoard();
                     CheckBoard(player2);
                 }
                 gameboard.changeTurn();
-                DisplayBoard();
             }
             else{
                 window.alert("Please input names for each player!");
             }
         });
     }
+    reset.addEventListener("click", function(){
+        gameboard.board = ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
+
+        DisplayPlayers();
+    });
+
+
 });
 
 // factory functions
@@ -88,11 +85,26 @@ function DisplayBoard(){
         playSpace[i].classList.add(`${gameboard.board[i]}`);
     }
 }
+function ResetBoard(){
+    const playSpace = document.querySelectorAll(".board");
+    for(let i = 0; i < gameboard.board.length; i++){
+        playSpace[i].classList.add(`${gameboard.board[i]}`);
+    }
+}
 
 function CheckBoard(player){
     // winner check
-    if(/******start here******/){
-        console.log(`${player.name} is the winner!`);
+    if( gameboard.board[0] == player.mark && gameboard.board[1] == player.mark && gameboard.board[2] == player.mark||
+        gameboard.board[3] == player.mark && gameboard.board[4] == player.mark && gameboard.board[5] == player.mark||
+        gameboard.board[6] == player.mark && gameboard.board[7] == player.mark && gameboard.board[8] == player.mark||
+        gameboard.board[0] == player.mark && gameboard.board[3] == player.mark && gameboard.board[6] == player.mark||
+        gameboard.board[1] == player.mark && gameboard.board[4] == player.mark && gameboard.board[7] == player.mark||
+        gameboard.board[2] == player.mark && gameboard.board[5] == player.mark && gameboard.board[8] == player.mark||
+        gameboard.board[0] == player.mark && gameboard.board[4] == player.mark && gameboard.board[8] == player.mark||
+        gameboard.board[2] == player.mark && gameboard.board[4] == player.mark && gameboard.board[6] == player.mark
+    ){
+        player.giveScore();
+        DisplayResults(`${player.name} is the winner!`);
     }
     // tie check
     let positionsLeft = 0;
@@ -102,6 +114,47 @@ function CheckBoard(player){
         }
     }
     if(positionsLeft <= 0){
-        console.log(`No more moves, tie game!`);
+        DisplayResults(`No more moves, tie game!`);
+    }
+}
+function DisplayResults(message){
+    const playerDisplay = document.querySelector(".player-display");
+    const playerResults = document.querySelector(".results-display");
+    const announceResult = document.querySelector(".result-annoucement");
+    const player1Result = document.querySelector(".player1.result");
+    const player2Result = document.querySelector(".player2.result");
+
+    playerDisplay.classList.toggle("hidden");
+    playerResults.classList.toggle("hidden");
+    announceResult.textContent = message;
+    player1Result.textContent = `Player 1 score: ${player1.getScore()}`;
+    player2Result.textContent = `Player 2 score: ${player2.getScore()}`;
+}
+
+function DisplayPlayers(){
+    const playerInput = document.querySelector(".player-input");
+    const playerDisplay = document.querySelector(".player-display");
+    const player1Display = document.querySelector(".player1");
+    const player2Display = document.querySelector(".player2");
+    const playerResults = document.querySelector(".results-display");
+    const player1Input = document.querySelector("#player1-input");
+    const player2Input = document.querySelector("#player2-input"); 
+    const button = document.querySelector(".submit");
+
+
+    // adding user input into UI
+    player1.name = player1Input.value;
+    player2.name = player2Input.value;
+    player1Display.textContent = "Player 1: " + player1.name;
+    player2Display.textContent = "Player 2: " + player2.name;
+    // hiding input elements
+    button.classList.toggle("hidden");  
+
+    playerDisplay.classList.toggle("hidden");
+    if(!playerResults.classList.contains("hidden")){
+        playerResults.classList.toggle("hidden");
+    }
+    if(!playerInput.classList.contains("hidden")){
+        playerInput.classList.toggle("hidden");
     }
 }
