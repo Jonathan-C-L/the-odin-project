ee8e88dc2a7971fb936cdd184eb68737d664a1f6
Jonathan-C-L <jle16@nait.ca>
2025-06-22 18:27:43 -0600

    messages display now and reset button is fully functional

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,14 +1,22 @@
-const gameboard = createGameboard();
+let gameboard = createGameboard();
 const player1 = createPlayer('', 'x');
 const player2 = createPlayer('', 'o');
 
-window.addEventListener("load", () =>{
-    const button = document.querySelector(".submit");
-    const player1Input = document.querySelector("#player1-input");
-    const player2Input = document.querySelector("#player2-input");    
-    const board = document.querySelectorAll(".board");
-    const reset = document.querySelector(".reset");
+const playerInput = document.querySelector(".player-input");
+const playerDisplay = document.querySelector(".player-display");
+const player1Display = document.querySelector(".player1");
+const player2Display = document.querySelector(".player2");
+const playerResults = document.querySelector(".results-display");
+const player1Input = document.querySelector("#player1-input");
+const player2Input = document.querySelector("#player2-input"); 
+const button = document.querySelector(".submit");
+const reset = document.querySelector(".reset");
+const announceResult = document.querySelector(".result-annoucement");
+const player1Result = document.querySelector(".player1.result");
+const player2Result = document.querySelector(".player2.result");
+const playSpace = document.querySelector(".gameboard");
 
+window.addEventListener("load", () =>{
     console.log("Welcome to tic tac toe!");
 
     button.addEventListener("click", function(){        
@@ -20,35 +28,29 @@ window.addEventListener("load", () =>{
             window.alert("Please input names for each player!")
         }
     });
-
-    // tic tac toe board 
-    for(let i = 0; i < board.length; i++){
-        board[i].addEventListener("click", function(){
-            if(gameboard.checkGame()){
-                if(gameboard.getTurn()%2 == 0){
-                    gameboard.pickPosition(i, player1);
-                    DisplayBoard();
-                    CheckBoard(player1);
-                }
-                else{
-                    gameboard.pickPosition(i, player2);
-                    DisplayBoard();
-                    CheckBoard(player2);
-                }
-                gameboard.changeTurn();
-            }
-            else{
-                window.alert("Please input names for each player!");
-            }
-        });
-    }
     reset.addEventListener("click", function(){
-        gameboard.board = ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
-
+        // gameboard.board = ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
+        gameboard = createGameboard();
+        gameboard.playGame();
+        ResetBoard();
         DisplayPlayers();
     });
-
-
+});
+// tic tac toe board 
+addGlobalEventListener("click", ".board", playSpace, e => {
+    if(gameboard.checkGame()){
+        if(gameboard.getTurn()%2 == 0){
+            gameboard.pickPosition(e.target.classList.value[0], player1);
+            DisplayBoard();
+            CheckBoard(player1);
+        }
+        else{
+            gameboard.pickPosition(e.target.classList.value[0], player2);
+            DisplayBoard();
+            CheckBoard(player2);
+        }
+        gameboard.changeTurn();
+    }
 });
 
 // factory functions
@@ -59,6 +61,7 @@ function createGameboard(){
     let play = false;
 
     const playGame = () => play = true;
+    const endGame = () => play = false;
     const checkGame = () => play;
     const changeTurn = () => playerTurn++;
     const getTurn = () => playerTurn;
@@ -66,7 +69,7 @@ function createGameboard(){
         (board[position] === '_')?board[position] = player.mark:console.log("Position is already taken!");
     };  
 
-    return {board, getTurn, changeTurn, pickPosition, playGame, checkGame};
+    return {board, getTurn, changeTurn, pickPosition, playGame, endGame, checkGame};
 }
 
 function createPlayer(name, mark){
@@ -80,15 +83,18 @@ function createPlayer(name, mark){
 
 // functions
 function DisplayBoard(){
-    const playSpace = document.querySelectorAll(".board");
+    const board = document.querySelectorAll(".board");
+
     for(let i = 0; i < gameboard.board.length; i++){
-        playSpace[i].classList.add(`${gameboard.board[i]}`);
+        board[i].classList.add(gameboard.board[i]);
     }
 }
 function ResetBoard(){
-    const playSpace = document.querySelectorAll(".board");
+    playSpace.innerHTML = "";
     for(let i = 0; i < gameboard.board.length; i++){
-        playSpace[i].classList.add(`${gameboard.board[i]}`);
+        let newBoard = document.createElement("div");
+        newBoard.setAttribute("class", `${i} board`);
+        playSpace.appendChild(newBoard);
     }
 }
 
@@ -105,6 +111,7 @@ function CheckBoard(player){
     ){
         player.giveScore();
         DisplayResults(`${player.name} is the winner!`);
+        gameboard.endGame();
     }
     // tie check
     let positionsLeft = 0;
@@ -115,15 +122,10 @@ function CheckBoard(player){
     }
     if(positionsLeft <= 0){
         DisplayResults(`No more moves, tie game!`);
+        gameboard.endGame();
     }
 }
 function DisplayResults(message){
-    const playerDisplay = document.querySelector(".player-display");
-    const playerResults = document.querySelector(".results-display");
-    const announceResult = document.querySelector(".result-annoucement");
-    const player1Result = document.querySelector(".player1.result");
-    const player2Result = document.querySelector(".player2.result");
-
     playerDisplay.classList.toggle("hidden");
     playerResults.classList.toggle("hidden");
     announceResult.textContent = message;
@@ -132,29 +134,28 @@ function DisplayResults(message){
 }
 
 function DisplayPlayers(){
-    const playerInput = document.querySelector(".player-input");
-    const playerDisplay = document.querySelector(".player-display");
-    const player1Display = document.querySelector(".player1");
-    const player2Display = document.querySelector(".player2");
-    const playerResults = document.querySelector(".results-display");
-    const player1Input = document.querySelector("#player1-input");
-    const player2Input = document.querySelector("#player2-input"); 
-    const button = document.querySelector(".submit");
-
-
     // adding user input into UI
     player1.name = player1Input.value;
     player2.name = player2Input.value;
     player1Display.textContent = "Player 1: " + player1.name;
     player2Display.textContent = "Player 2: " + player2.name;
     // hiding input elements
-    button.classList.toggle("hidden");  
-
+  
     playerDisplay.classList.toggle("hidden");
     if(!playerResults.classList.contains("hidden")){
         playerResults.classList.toggle("hidden");
     }
     if(!playerInput.classList.contains("hidden")){
         playerInput.classList.toggle("hidden");
+        button.classList.toggle("hidden");  
     }
+}
+// event delegation function
+function addGlobalEventListener(type, selector, parent = document, callback){
+
+    parent.addEventListener(type, e =>{
+        if(e.target.matches(selector)){
+            callback(e);
+        }
+    });
 }
