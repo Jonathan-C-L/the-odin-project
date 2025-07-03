8dea1d489ac589044718e6577bef1310a28718a2
Jonathan <jle16@nait.ca>
2025-07-02 22:10:36 -0600

    modules working now - now to need to build out html content

---
+++ restaurant-page/src/image-carousel.js
@@ -0,0 +1,8 @@
+export {ImageCarousel};
+
+const body = document.querySelector("#content");
+
+function ImageCarousel(){
+    body.textContent = "HELLO THERE";
+    console.log("content js is working");
+}
--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,4 +1,6 @@
 import {test} from "./test.js";
 import "./styles.css";
+import {ImageCarousel} from "./image-carousel.js";
 
 console.log(test);
+ImageCarousel();
