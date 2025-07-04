f6ade83a80990e20172c4bc22e0f0a05c0768b7d
Jonathan <jle16@nait.ca>
2025-07-04 14:15:07 -0600

    got images to preload into the array properly!

--- restaurant-page/src/image-carousel.js
+++ restaurant-page/src/image-carousel.js
@@ -1,5 +1,5 @@
 // exports
-export {ImageCarousel, images};
+export {ImageCarousel};
 import food1 from "./images/food1.jpg";
 import food2 from "./images/food2.jpg";
 import food3 from "./images/food3.jpg";
@@ -7,25 +7,24 @@ import food4 from "./images/food4.jpg";
 
 // variables
 let images = [];
-const body = document.querySelector("#content");
+const img = document.createElement("img");
+    const body = document.querySelector("#content");
+
 
 // factory functions
-function NewImage(name, src){
+function NewImage(src){
     let image = new Image();
     image.src = src;
-    return {name, image};
+    return {image};
 }
 
 
 function ImageCarousel(){
     body.textContent = "HELLO THERE";
     console.log("content js is working");
-    LoadImages(4);
-}
-
-function LoadImages(imageCount){
-    images.push(food1);
-    images.push(food2);
-    images.push(food3);
-    images.push(food4);
+    
+    
+    let newImage = new Image();
+    newImage.src = food1;
+    images.push(newImage);
 }
