a5e628e895edaddfae19aaa9e988c88d1f2b0764
Jonathan <jle16@nait.ca>
2025-07-03 20:51:11 -0600

    figuring out how to load images for an image carousel

--- restaurant-page/src/image-carousel.js
+++ restaurant-page/src/image-carousel.js
@@ -1,8 +1,23 @@
+// exports
 export {ImageCarousel};
 
+// variables
+let images = [];
 const body = document.querySelector("#content");
+
+// factory functions
+function NewImage(name, src){
+    let image = new Image();
+    image.src = src;
+    return {name, image};
+}
+
 
 function ImageCarousel(){
     body.textContent = "HELLO THERE";
     console.log("content js is working");
+}
+
+function LoadImages(){
+    
 }
